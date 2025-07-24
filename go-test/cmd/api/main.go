c6f9a0422f74957feb8d99f01b5b729aaacd335f
Javi Medina <javier_j_medina@hotmail.com>
2025-07-24 18:31:11 -0300

    dockerfile

--- go-test/cmd/api/main.go
+++ go-test/cmd/api/main.go
@@ -10,22 +10,16 @@ import (
 )
 
 func main() {
-	// Inicializar productos
 	if err := handlers.SetupProducts("."); err != nil {
 		log.Fatalf("Error cargando productos: %v", err)
 	}
 
-	// Configurar Gin
 	r := gin.Default()
 
-	// Middlewares globales
 	r.Use(middleware.CORS())
 	r.Use(middleware.Cache())
-
-	// Rutas
 	r.GET("/search", handlers.SearchHandler)
 
-	// Iniciar servidor
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("Error iniciando servidor: %v", err)
 	}
